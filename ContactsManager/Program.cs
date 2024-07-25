d454331447b759703f28ee5dc9c543ebf64f534f
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-25 16:21:10 +0700

    Initilize middlewares and css file

--- ContactsManager/Program.cs
+++ ContactsManager/Program.cs
@@ -5,9 +5,16 @@ namespace ContactsManager
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllers();
 
             app.Run();
         }
