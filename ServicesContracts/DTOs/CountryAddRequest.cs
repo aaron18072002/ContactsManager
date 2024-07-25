01786096df7346c09c88be760b218e6619e5e071
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-25 16:59:02 +0700

    Create a new DTO class for adding a new country

---
+++ ServicesContracts/DTOs/CountryAddRequest.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+
+namespace ServicesContracts.DTOs
+{
+    public class CountryAddRequest
+    {
+        /// <summary>
+        /// DTO class for add a new country
+        /// </summary>
+        public string? CountryName { get; set; }
+        public Country ToCountry()
+        {
+            return new Country()
+            {
+                CountryName = this.CountryName
+            };
+        }
+    }
+}
