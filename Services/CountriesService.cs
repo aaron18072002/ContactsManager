e894d9200abb6dbd32dcf37fcac63ea5ea6f016b
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-25 17:37:50 +0700

    Create CountriesService

---
+++ Services/CountriesService.cs
@@ -0,0 +1,13 @@
+﻿using ServicesContracts.DTOs;
+using ServicesContracts.Interfaces;
+
+namespace Services
+{
+    public class CountriesService : ICountriesService
+    {
+        public CountryResponse AddCountry(CountryAddRequest country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
