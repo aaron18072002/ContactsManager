ad95ad415da329a8329ffe6d1f91bebc7f8bf8d2
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-25 17:26:52 +0700

    Create an interface to implement DI for CountriesService

---
+++ ServicesContracts/Interfaces/ICountriesService.cs
@@ -0,0 +1,22 @@
+﻿using ServicesContracts.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesContracts.Interfaces
+{
+    /// <summary>
+    /// Represents business logics for manipulating with Country entity
+    /// </summary>
+    public interface ICountriesService
+    {
+        /// <summary>
+        /// Add a country object to list of countries
+        /// </summary>
+        /// <param name="country">Country object to add</param>
+        /// <returns>Returns the country object after adding it (including newly generated country id)</returns>
+        CountryResponse AddCountry(CountryAddRequest country);
+    }
+}
