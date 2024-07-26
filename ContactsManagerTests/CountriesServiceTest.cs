61e3e0ef554ec2c7e57e5bb6d9f989ce7076caa0
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-26 10:55:33 +0700

    Implement unit test for AddCountry

--- ContactsManagerTests/CountriesServiceTest.cs
+++ ContactsManagerTests/CountriesServiceTest.cs
@@ -1,4 +1,5 @@
 using Services;
+using ServicesContracts.DTOs;
 using ServicesContracts.Interfaces;
 
 namespace ContactsManagerTests
@@ -10,10 +11,79 @@ namespace ContactsManagerTests
         {
             this._countriesService = new CountriesService();
         }
-        [Fact]
-        public void Test1()
-        {
 
+        [Fact]
+        public void AddCountry_NullCountry()
+        {
+            //Arrange
+            CountryAddRequest? request = null;
+
+            //Act
+            var action = () =>
+            {
+                this._countriesService.AddCountry(request);
+            };
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void AddCountry_CountryNameIsNull()
+        {
+            //Arrange
+            var request = new CountryAddRequest()
+            {
+                CountryName = null
+            };
+
+            //Act
+            var action = () =>
+            {
+                this._countriesService.AddCountry(request);
+            };
+
+            //Assert
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            //Arrange
+            this._countriesService.AddCountry(new CountryAddRequest()
+            {
+                CountryName = "Japan"
+            });
+            var request = new CountryAddRequest()
+            {
+                CountryName = "Japan"
+            };
+
+            //Act
+            var action = () =>
+            {
+                this._countriesService.AddCountry(request);
+            };
+
+            //Assert
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void AddCountry_ProperCountryDetails()
+        {
+            //Arrange
+            var request = new CountryAddRequest()
+            {
+                CountryName = "Japan"
+            };
+
+            //Act
+            var response = this._countriesService.AddCountry(request);
+
+            //Assert
+            Assert.True(response.CountryId != Guid.Empty);
         }
     }
 }
