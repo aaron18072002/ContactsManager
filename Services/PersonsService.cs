f673908d60ed2af0638e661888bc195e05d69842
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-30 16:20:04 +0700

    Implement unit test for AddPerson method

---
+++ Services/PersonsService.cs
@@ -0,0 +1,23 @@
+﻿using ServicesContracts.DTOs;
+using ServicesContracts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class PersonsService : IPersonsService
+    {
+        public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PersonResponse> GetAllPersons()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
