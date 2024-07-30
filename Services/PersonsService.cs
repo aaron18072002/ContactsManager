8a652deef7a9a31a866862c13326dc8b0a84d2eb
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-30 16:57:46 +0700

    Create IPersonsService and implement functionality for AddPerson method

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -1,4 +1,5 @@
-﻿using ServicesContracts.DTOs;
+﻿using Entities;
+using ServicesContracts.DTOs;
 using ServicesContracts.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,40 @@ namespace Services
 {
     public class PersonsService : IPersonsService
     {
+        private List<Person> _people;
+        private readonly ICountriesService _countriesService;
+        public PersonsService()
+        {
+            this._people = new List<Person>();
+            this._countriesService = new CountriesService();
+        }
+        private PersonResponse ConvertPersonToPersonResponse(Person person)
+        {
+            var personResponse = person.ToPersonResponse();
+            personResponse.CountryName = this._countriesService.GetCountryByCountryId
+                (personResponse.CountryId)?.CountryName;
+
+            return personResponse;
+        }
         public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
         {
-            throw new NotImplementedException();
+            if(personAddRequest is null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest));
+            }
+            if(personAddRequest.PersonName is null)
+            {
+                throw new ArgumentException("PersonName cant be null");
+            }
+
+            var personEntity = personAddRequest.ToPerson();
+            personEntity.PersonId = Guid.NewGuid();
+
+            this._people.Add(personEntity);
+
+            var personResponse = this.ConvertPersonToPersonResponse(personEntity);
+
+            return personResponse;
         }
 
         public List<PersonResponse> GetAllPersons()
---
+++ ServicesContracts/Interfaces/IPersonsService.cs
@@ -0,0 +1,25 @@
+﻿using ServicesContracts.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesContracts.Interfaces
+{
+    public interface IPersonsService
+    {
+        /// <summary>
+        /// Add a person object to list of persons
+        /// </summary>
+        /// <param name="personAddRequest">A person object to add</param>
+        /// <returns>Returns the PersonResponse after adding it (including newly generated person id)</returns>
+        PersonResponse AddPerson(PersonAddRequest? personAddRequest);
+
+        /// <summary>
+        /// Return all people from datasource
+        /// </summary>
+        /// <returns>Returns list of person response object</returns>
+        List<PersonResponse> GetAllPersons();
+    }
+}
