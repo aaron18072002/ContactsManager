7f6b514cdd8e8be70a918dc3936d8faf91f1e426
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-31 10:24:53 +0700

    Add validation attribute for PersonAddRequest

--- ServicesContracts/DTOs/PersonAddRequest.cs
+++ ServicesContracts/DTOs/PersonAddRequest.cs
@@ -2,6 +2,7 @@
 using ServicesContracts.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,23 @@ namespace ServicesContracts.DTOs
 {
     public class PersonAddRequest
     {
+        [Required(ErrorMessage = "PersonName cant be null or empty")]
         public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email cant be null or empty")]
+        [EmailAddress(ErrorMessage = "Email value must be a valid email")]
         public string? Email { get; set; }
+
         public DateTime? DateOfBirth { get; set; }
+
         public GenderOptions? Gender { get; set; }
+
         public Guid? CountryId { get; set; }
+
         public string? Address { get; set; }
+
         public bool ReceiveNewsLetters { get; set; }
+
         public Person ToPerson()
         {
             return new Person()
