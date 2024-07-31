9c7ac80d22c313d02ae14ff1429abe7f0f5b8cff
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-07-31 10:26:18 +0700

    Create functionality for Model Validation

---
+++ Services/Helpers/ValidationsHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Helpers
+{
+    public class ValidationsHelper
+    {
+        public static void ModelVaidation(object model)
+        {
+            var validationContext = new ValidationContext(model);
+            var validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject
+                (model, validationContext, validationResults, true);
+            if(!isValid)
+            {
+                throw new ArgumentException(validationResults?.FirstOrDefault()?.ErrorMessage);
+            }
+        }
+    }
+}
