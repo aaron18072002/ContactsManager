7ae7fe56ee0ba1d49c7db82d5ce4ac067d3369ea
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-01 15:54:38 +0700

    Initialize Persons controller

---
+++ ContactsManager/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("/")]
+        [Route("persons/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
