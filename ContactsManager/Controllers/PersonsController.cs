62f11203e2a52cbf9df35b1dc406521d4a82df72
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-02 10:28:28 +0700

    Implement controller and view for render list of persons

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServicesContracts.Interfaces;
 
 namespace ContactsManager.Controllers
 {
     public class PersonsController : Controller
     {
+        private IPersonsService _personsService;
+        public PersonsController(IPersonsService personsService)
+        {
+            this._personsService = personsService;
+        }
+
         [Route("/")]
         [Route("persons/index")]
         public IActionResult Index()
         {
-            return View();
+            var allPersons = this._personsService.GetAllPersons();
+
+            return View(allPersons);
         }
     }
 }
