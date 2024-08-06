1ed0bad6905058fafc65cb4f93e825381015a0c4
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-06 16:16:08 +0700

    Add annotations for mapping types to SqlServer

--- Entities/Country.cs
+++ Entities/Country.cs
@@ -1,11 +1,17 @@
-﻿namespace Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
 {
     public class Country
     {
         /// <summary>
         /// Domain model for Country
         /// </summary>
+ 
+        [Key]             
         public Guid CountryId { get; set; }
+
+        [StringLength(40)]
         public string? CountryName { get; set; }
     }
 }
--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,28 @@ namespace Entities
 {
     public class Person
     {
+        [Key]
         public Guid? PersonId { get; set; }
+
+        [StringLength(40)]
+        [Column("PersonName")]
         public string? PersonName { get; set; }
+
+        [StringLength(40)]
         public string? Email { get; set; }
+
         public DateTime? DateOfBirth { get; set; }
+
+        [StringLength(10)]
         public string? Gender { get; set; }
+
+        //uniqueindentifier
         public Guid? CountryId { get; set; }
+
+        [StringLength(100)]
         public string? Address { get; set; }
+
+        //bit
         public bool ReceiveNewsLetters { get; set; }
     }
 }
