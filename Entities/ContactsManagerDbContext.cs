b1870e9046da0bda89adaeccc9e80ef0f94cab19
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-06 17:09:52 +0700

    Read data from json files to seed database

--- Entities/ContactsManagerDbContext.cs
+++ Entities/ContactsManagerDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Entities
@@ -16,6 +17,44 @@ namespace Entities
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().ToTable("Countries");
             modelBuilder.Entity<Person>().ToTable("Persons");
+
+            //Seed data for Persons table
+            var personsFromJson = new StringBuilder();
+            using(var streamReader = new StreamReader("persons.json"))
+            {
+                string? line;
+                while ((line = streamReader.ReadLine()) is not null)
+                {
+                    personsFromJson.AppendLine(line);
+                }
+            }
+            var listPersons = JsonSerializer.Deserialize<List<Person>>(personsFromJson.ToString());
+            if(listPersons is not null)
+            {
+                foreach (var person in listPersons)
+                {
+                    modelBuilder.Entity<Person>().HasData(person); 
+                }
+            }
+
+            //Seed data for Countries table
+            var countriesFromJson = new StringBuilder();
+            using(var streamReader = new StreamReader("countries.json"))
+            {
+                string? line;
+                while((line = streamReader.ReadLine()) is not null)
+                {
+                    countriesFromJson.AppendLine(line);
+                }
+            }
+            var listCountries = JsonSerializer.Deserialize<List<Country>>(countriesFromJson.ToString());
+            if( listCountries is not null)
+            {
+                foreach (var country in listCountries)
+                {
+                    modelBuilder.Entity<Country>().HasData(country);
+                }
+            }
         }
     }
 }
