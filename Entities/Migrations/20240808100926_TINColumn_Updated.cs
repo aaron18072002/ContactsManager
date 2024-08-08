600ce55ff97127b670de6733a5260468cbc644c0
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-08 17:12:32 +0700

    Apply migration files into database

---
+++ Entities/Migrations/20240808100926_TINColumn_Updated.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class TINColumn_Updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TIN",
+                table: "Persons",
+                type: "varchar(8)",
+                nullable: true,
+                defaultValue: "ABC12345",
+                oldClrType: typeof(string),
+                oldType: "varchar(8)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TIN",
+                table: "Persons",
+                type: "varchar(8)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(8)",
+                oldNullable: true,
+                oldDefaultValue: "ABC12345");
+        }
+    }
+}
