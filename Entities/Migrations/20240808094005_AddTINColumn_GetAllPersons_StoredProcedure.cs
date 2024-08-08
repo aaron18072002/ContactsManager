685ce192e9919ae0a5d015a5025b79041f789170
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-08 16:45:07 +0700

    Apply migration files about AddTINColumn into database

---
+++ Entities/Migrations/20240808094005_AddTINColumn_GetAllPersons_StoredProcedure.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class AddTINColumn_GetAllPersons_StoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string getAllPersonsSP =
+                @"
+                    CREATE PROCEDURE [dbo].[GetAllPersons]
+                    AS BEGIN
+                        SELECT [PersonId]
+                               ,[PersonName]
+                               ,[Email]
+                               ,[DateOfBirth]
+                               ,[Gender]
+                               ,[CountryId]
+                               ,[Address]
+                               ,[ReceiveNewsLetters]
+                               ,[TIN]
+                        FROM [dbo].[Persons]
+                    END
+                ";
+            migrationBuilder.Sql(getAllPersonsSP);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string dropGetAllPersonsSP =
+               @"
+                    DROP PROCEDURE [dbo].[GetAllPersons]
+                ";
+            migrationBuilder.Sql(dropGetAllPersonsSP);
+        }
+    }
+}
