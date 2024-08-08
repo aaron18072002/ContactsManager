11148fd052c89ecd2f221108037e74e1faa3c46f
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-08 09:56:40 +0700

    Convert in-memory db to SqlServer db

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -6,40 +6,10 @@ namespace Services
 {
     public class CountriesService : ICountriesService
     {
-        private List<Country> _countries;
-        public CountriesService(bool initialize = true)
+        private readonly ContactsManagerDbContext _db;
+        public CountriesService(ContactsManagerDbContext contactsManagerDbContext)
         {
-            this._countries = new List<Country>();
-            if(initialize)
-            {
-                this._countries.AddRange(new List<Country>()
-                {
-                    new Country()
-                    {
-                        CountryId = Guid.Parse("000C76EB-62E9-4465-96D1-2C41FDB64C3B"), 
-                        CountryName = "USA"
-                    },
-                    new Country() 
-                    { 
-                        CountryId = Guid.Parse("32DA506B-3EBA-48A4-BD86-5F93A2E19E3F"), 
-                        CountryName = "Canada" 
-                    },
-                    new Country() 
-                    { 
-                        CountryId = Guid.Parse("DF7C89CE-3341-4246-84AE-E01AB7BA476E"), 
-                        CountryName = "UK" 
-                    },
-                    new Country() 
-                    { 
-                        CountryId = Guid.Parse("15889048-AF93-412C-B8F3-22103E943A6D"), 
-                        CountryName = "India" 
-                    },
-                    new Country() 
-                    { 
-                        CountryId= Guid.Parse("80DF255C-EFE7-49E5-A7F9-C35D7C701CAB"), 
-                        CountryName = "Australia" }
-                });
-            }
+            this._db = contactsManagerDbContext; 
         }
         public CountryResponse AddCountry(CountryAddRequest? country)
         {
@@ -53,7 +23,7 @@ namespace Services
                 throw new ArgumentException(nameof(country.CountryName));
             }
 
-            if(this._countries.Where(c => c.CountryName == country.CountryName).Count() > 0)
+            if(this?._db.Countries?.Count(c => c.CountryName == country.CountryName) > 0)
             {
                 throw new ArgumentException("This CountryName already exists");
             }
@@ -61,15 +31,17 @@ namespace Services
             var countryEntity = country.ToCountry();
             countryEntity.CountryId = Guid.NewGuid();
 
-            this._countries.Add(countryEntity);
+            this?._db?.Countries?.Add(countryEntity);
+            this._db.SaveChanges();
 
             return countryEntity.ToCountryResponse();
         }
         public List<CountryResponse> GetAllCountries()
         {
-            var result = this._countries.Select(c => c.ToCountryResponse()).ToList();
+            var result = this?._db?.Countries?.ToList()
+                .Select(c => c.ToCountryResponse()).ToList();
 
-            return result;
+            return result ?? new List<CountryResponse>();
         }
         public CountryResponse? GetCountryByCountryId(Guid? id)
         {
@@ -78,7 +50,7 @@ namespace Services
                 return null;
             }
 
-            var countryEntity = this._countries.FirstOrDefault
+            var countryEntity = this?._db?.Countries?.FirstOrDefault
                 (c => c.CountryId == id);
 
             return countryEntity == null ? null : countryEntity.ToCountryResponse();
