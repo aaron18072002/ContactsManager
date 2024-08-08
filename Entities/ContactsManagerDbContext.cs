fe116726a459dbffc48f79ebaca2f49ef323c172
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-08 12:00:36 +0700

    Add InsertPerson SP to SqlServer db

--- Entities/ContactsManagerDbContext.cs
+++ Entities/ContactsManagerDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -66,6 +67,26 @@ namespace Entities
             var result = this?.Persons?.FromSqlRaw("EXECUTE [dbo].[GetAllPersons]").ToList();
 
             return result ?? new List<Person>();
+        }
+
+        public int sp_InsertPerson(Person person)
+        {
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@PersonId", person.PersonId),
+                new SqlParameter("@PersonName", person.PersonName),
+                new SqlParameter("@Email", person.Email),
+                new SqlParameter("@DateOfBirth", person.DateOfBirth),
+                new SqlParameter("@Gender", person.Gender),
+                new SqlParameter("@CountryId", person.CountryId),
+                new SqlParameter("@Address", person.Address),
+                new SqlParameter("@ReceiveNewsLetters", person.ReceiveNewsLetters),
+            };
+
+            var result = this.Database.ExecuteSqlRaw
+                ("EXECUTE [dbo].[InsertPerson] @PersonId, @PersonName, @Email, @DateOfBirth, @Gender, @CountryId, @Address, @ReceiveNewsLetters", parameters);
+
+            return result;
         }
     }
 }
---
+++ Entities/Migrations/20240808043336_InsertPerson_StoredProcedure.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class InsertPerson_StoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var insertPersonSP = @"
+                CREATE PROCEDURE [dbo].[InsertPerson]
+                (@PersonId uniqueidentifier, @PersonName nvarchar(40),
+                 @Email nvarchar(40), @DateOfBirth datetime2(7),
+                 @Gender nvarchar(10), @CountryId uniqueidentifier, 
+                 @Address nvarchar(100), @ReceiveNewsLetters bit)
+                AS BEGIN
+                    INSERT INTO [dbo].[Persons] (PersonId, PersonName, Email, DateOfBirth, Gender, CountryId, Address, ReceiveNewsLetters)
+                    VALUES (@PersonId, @PersonName, @Email, @DateOfBirth, @Gender, @CountryId, @Address, @ReceiveNewsLetters);
+                END
+            ";
+            migrationBuilder.Sql(insertPersonSP);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var dropInsertPersonSP = @"DROP PROCEDURE [dbo].[InsertPerson]";
+            migrationBuilder.Sql(dropInsertPersonSP);
+        }
+    }
+}
