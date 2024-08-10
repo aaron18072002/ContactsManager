f72958cc75d1de88f86ac8d521364e44bb363424
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-10 10:26:13 +0700

    Create endpoint for UploadCountriesFromExcel functionality

---
+++ ContactsManager/Controllers/CountriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.Controllers
+{
+    [Route("[controller]")]
+    public class CountriesController : Controller
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult UploadFromExcel()
+        {
+            return View();
+        }
+    }
+}
