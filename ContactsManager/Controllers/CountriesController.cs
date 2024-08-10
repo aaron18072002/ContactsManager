8396e1bc6a9b225b35230a07752e9f3a35faa38e
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-10 10:32:56 +0700

    Create endpoint for submit excel form

--- ContactsManager/Controllers/CountriesController.cs
+++ ContactsManager/Controllers/CountriesController.cs
@@ -1,14 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServicesContracts.Interfaces;
 
 namespace ContactsManager.Controllers
 {
     [Route("[controller]")]
     public class CountriesController : Controller
     {
+        private readonly ICountriesService _countriesService;
+        public CountriesController(ICountriesService countriesService)
+        {
+            this._countriesService = countriesService;
+        }
+
         [HttpGet]
         [Route("[action]")]
         public IActionResult UploadFromExcel()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> UploadFromExcel
+            ([FromForm]IFormFile excelFile)
+        {
+            if (excelFile == null || excelFile.Length == 0)
+            {
+                ViewBag.ErrorMessage = "Please select an xlsx file";
+                return View();
+            }
+            if (!Path.GetExtension(excelFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                ViewBag.ErrorMessage = "Unsupported file. 'xlsx' file is expected";
+                return View();
+            }
+            int countriesCountInserted = await this._countriesService.UploadCountriesFromExcelFile(excelFile);
+
+            ViewBag.Message = $"{countriesCountInserted} Countries Uploaded";
             return View();
         }
     }
