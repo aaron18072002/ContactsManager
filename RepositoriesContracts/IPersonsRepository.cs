a91cbb18a95a3b29d61b0a0a39177cfdde90ffca
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-12 10:26:45 +0700

    Create repository contracts

---
+++ RepositoriesContracts/IPersonsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoriesContracts
+{
+    public interface IPersonsRepository
+    {
+        Task<Person> AddPerson(Person person);
+
+        Task<List<Person>> GetAllPersons();
+
+        Task<Person> GetPersonById(Guid personId);
+
+        Task<List<Person>?> GetFilteredPersons(Expression<Func<Person, bool>> predicate);
+
+        Task<bool> DeletePerson(Guid personId);
+
+        Task<Person> UpdatePerson(Person person);
+    }
+}
