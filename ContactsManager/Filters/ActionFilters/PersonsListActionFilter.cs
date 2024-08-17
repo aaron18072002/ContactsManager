5e95990c289034e6727e0d256a2a10e8e9314f37
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-17 11:10:41 +0700

    Create filter for Index action method

---
+++ ContactsManager/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ActionFilters
+{
+    public class PersonsListActionFilter : IActionFilter
+    {
+        private readonly ILogger<PersonsListActionFilter> _logger;
+        public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+        {
+            this._logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            this._logger.LogInformation("OnActionExecuted method of PersonsListActionFilter");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            this._logger.LogInformation("OnActionExecuting method of PersonsListActionFilter");
+        }
+    }
+}
