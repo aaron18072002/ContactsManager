caddc8c2989fb93dadd454d002077df3234e5c1f
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-17 15:09:01 +0700

    Create ActionFilter for config response header

---
+++ ContactsManager/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ActionFilters
+{
+    public class ResponseHeaderActionFilter : IActionFilter
+    {
+        private readonly ILogger<ResponseHeaderActionFilter> _logger;
+        private readonly string _key;
+        private readonly string _value;
+        public ResponseHeaderActionFilter
+            (ILogger<ResponseHeaderActionFilter> logger, string key, string value)
+        {
+            this._logger = logger;
+            this._key = key;
+            this._value = value;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            this._logger.LogInformation("{FilterName}.{MethodName} method", 
+                nameof(ResponseHeaderActionFilter), nameof(this.OnActionExecuted));
+            context.HttpContext.Response.Headers[this._key] = this._value;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            this._logger.LogInformation("{FilterName}.{MethodName} method",
+                nameof(ResponseHeaderActionFilter), nameof(this.OnActionExecuting));
+        }
+    }
+}
