78c412fef7aa675699b082840baa5b3e9db2a180
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-19 17:01:12 +0700

    Create ResourceFilter to disable some action method

---
+++ ContactsManager/Filters/ResourceFilters/FeatureDisabledResourceFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ResourceFilters
+{
+    public class FeatureDisabledResourceFilter : IAsyncResourceFilter
+    {
+        private readonly ILogger<FeatureDisabledResourceFilter> _logger;
+        private bool _isDisabled;
+        public FeatureDisabledResourceFilter(ILogger<FeatureDisabledResourceFilter> logger, bool isDisabled)
+        {
+            this._logger = logger;
+            this._isDisabled = isDisabled;
+        }
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            this._logger.LogInformation("{FilterName}.{MethodName} method before",
+                nameof(FeatureDisabledResourceFilter), nameof(this.OnResourceExecutionAsync));
+
+            if (this._isDisabled)
+            {
+                context.Result = new StatusCodeResult(501); //not implemented
+            }
+            else
+            {
+                await next();
+            }
+
+            this._logger.LogInformation("{FilterName}.{MethodName} method after",
+                nameof(FeatureDisabledResourceFilter), nameof(this.OnResourceExecutionAsync));
+        }
+    }
+}
