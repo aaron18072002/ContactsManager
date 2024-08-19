aa48295b4d4e9f70dbecfbda59d2a532682b85bc
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-19 14:47:07 +0700

    Create filter to short-circuiting when model binding is invalid

---
+++ ContactsManager/Filters/ActionFilters/PersonCreateAndEditPostActionFilter.cs
@@ -0,0 +1,48 @@
+﻿using ContactsManager.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ServicesContracts.Interfaces;
+
+namespace ContactsManager.Filters.ActionFilters
+{
+    public class PersonCreateAndEditPostActionFilter : IAsyncActionFilter
+    {
+        private readonly ILogger<PersonCreateAndEditPostActionFilter> _logger;
+        private readonly ICountriesService _countriesService;
+        private string _personRequestName;
+        public PersonCreateAndEditPostActionFilter
+            (ILogger<PersonCreateAndEditPostActionFilter> logger,ICountriesService countriesService ,string personRequestName)
+        {
+            this._logger = logger;
+            this._countriesService = countriesService;
+            this._personRequestName = personRequestName;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            this._logger.LogInformation("{FilterName}.{MethodName} method",
+                nameof(PersonsListActionFilter), nameof(this.OnActionExecutionAsync));
+            if(context.Controller is PersonsController)
+            {
+                var personsController = context.Controller as PersonsController;
+                if(personsController is not null && !personsController.ModelState.IsValid)
+                {
+                    var countries = await this._countriesService.GetAllCountries();
+
+                    personsController.ViewBag.Countries = countries;
+                    personsController.ViewBag.Errors = personsController.ModelState.Values
+                        .SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+
+                    var personRequest = context.ActionArguments[this._personRequestName];
+
+                    context.Result = personsController.View(personRequest);
+                } else
+                {
+                    await next();
+                }
+            } else
+            {
+                await next();
+            }
+        }
+    }
+}
