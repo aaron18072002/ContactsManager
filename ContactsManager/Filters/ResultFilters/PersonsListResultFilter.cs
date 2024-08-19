e50af967108866df07925a7b5ae27aefdd39b3fe
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-19 15:34:30 +0700

    Create ResultFilter for apply into Persons Index method

---
+++ ContactsManager/Filters/ResultFilters/PersonsListResultFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ResultFilters
+{
+    public class PersonsListResultFilter : IAsyncResultFilter
+    {
+        private readonly ILogger<PersonsListResultFilter> _logger;
+        public PersonsListResultFilter(ILogger<PersonsListResultFilter> logger)
+        {
+            this._logger = logger;
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            this._logger.LogInformation("{FilterName}.{MethodName} method before",
+                nameof(PersonsListResultFilter), nameof(this.OnResultExecutionAsync));
+            context.HttpContext.Response.Headers["Last-Modifed"] = DateTime.Now.ToString("dd-MM-yyyy-hh-mm");
+
+            await next();
+
+            this._logger.LogInformation("{FilterName}.{MethodName} method after",
+                nameof(PersonsListResultFilter), nameof(this.OnResultExecutionAsync));          
+        }
+    }
+}
