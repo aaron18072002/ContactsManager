5376c5888ee6d955f39b662b1e160ab992d2b5c3
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-21 00:07:16 +0700

    Create AlwaysRunResultFilter

---
+++ ContactsManager/Filters/ResultFilters/PersonAlwaysRunResultFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ResultFilters
+{
+    public class PersonAlwaysRunResultFilter : IAlwaysRunResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            if (context.Filters.OfType<SkipFilter>().Any())
+            {
+                return;
+            }
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            if(context.Filters.OfType<SkipFilter>().Any())
+            {
+                return;
+            }
+        }
+    }
+}
