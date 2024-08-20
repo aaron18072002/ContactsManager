328e01c9a51f796baca9e04feb1bc77d7a25fa2d
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-20 21:49:01 +0700

    Create ResultFilter to add cookie in response

---
+++ ContactsManager/Filters/ResultFilters/TokenResultFilter.cs
@@ -0,0 +1,26 @@
+﻿using ContactsManager.Filters.ResourceFilters;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.ResultFilters
+{
+    public class TokenResultFilter : IAsyncResultFilter
+    {
+        private readonly ILogger<TokenResultFilter> _logger;
+        public TokenResultFilter(ILogger<TokenResultFilter> logger)
+        {
+            this._logger = logger;
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            this._logger.LogInformation("{FilterName}.{MethodName} method before",
+                nameof(TokenResultFilter), nameof(this.OnResultExecutionAsync));
+            context.HttpContext.Response.Cookies.Append("Auth-Key", "Auth-Value");
+
+            await next();
+
+            this._logger.LogInformation("{FilterName}.{MethodName} method after",
+                nameof(TokenResultFilter), nameof(this.OnResultExecutionAsync));           
+        }
+    }
+}
