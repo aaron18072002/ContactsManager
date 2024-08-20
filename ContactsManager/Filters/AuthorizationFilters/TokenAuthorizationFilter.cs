e5e35747a1aeb3c81720c852521c58eb7ca66434
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-20 21:48:17 +0700

    Create AuthorizationFilter to add authorize cookie from request

---
+++ ContactsManager/Filters/AuthorizationFilters/TokenAuthorizationFilter.cs
@@ -0,0 +1,31 @@
+﻿using ContactsManager.Filters.ResourceFilters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ContactsManager.Filters.AuthorizationFilters
+{
+    public class TokenAuthorizationFilter : IAuthorizationFilter
+    {
+        private readonly ILogger<TokenAuthorizationFilter> _logger;
+        public TokenAuthorizationFilter(ILogger<TokenAuthorizationFilter> logger)
+        {
+            this._logger = logger;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            this._logger.LogInformation("{FilterName}.{MethodName} method",
+                nameof(TokenAuthorizationFilter), nameof(this.OnAuthorization));
+            if(context.HttpContext.Request.Cookies.ContainsKey("Auth-Key") == false)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+                return;
+            }
+            if (context.HttpContext.Request.Cookies["Auth-Key"] == "Auth-Value")
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+                return;
+            }
+        }
+    }
+}
