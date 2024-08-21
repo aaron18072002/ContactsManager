c11a8a3745d11cc288332c8cae793b3227d68f4d
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-21 15:44:59 +0700

    Create a static class to add all needed services

---
+++ ContactsManager/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,61 @@
+﻿using ContactsManager.Filters.ActionFilters;
+using ContactsManager.Filters.ResultFilters;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoriesContracts;
+using Services;
+using ServicesContracts.Interfaces;
+
+namespace ContactsManager.StartupExtensions
+{
+    public static class ConfigureServicesExtension
+    {
+        public static IServiceCollection ConfigureServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+                logging.RequestHeaders.Add("sec-ch-ua");
+                logging.ResponseHeaders.Add("MyResponseHeader");
+                logging.MediaTypeOptions.AddText("application/javascript");
+                logging.RequestBodyLogLimit = 4096;
+                logging.ResponseBodyLogLimit = 4096;
+                logging.CombineLogs = true;
+            });
+
+            services.AddTransient<ResponseHeaderActionFilter>();
+
+            services.AddControllersWithViews(options =>
+            {
+                //options.Filters.Add<ResponseHeaderActionFilter>(5); without any parameters
+
+                var logger = services.BuildServiceProvider()
+                    .GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+
+                options.Filters.Add(new ResponseHeaderActionFilter(logger)
+                {
+                    Key = "My-Key-From-Global",
+                    Value = "My-Key-From-Global",
+                    Order = 2
+                });
+
+                options.Filters.Add<PersonAlwaysRunResultFilter>();
+            });
+
+            services.AddScoped<ICountriesService, CountriesService>();
+            services.AddScoped<IPersonsService, PersonsService>();
+
+            services.AddScoped<ICountriesRepository, CountriesRepository>();
+            services.AddScoped<IPersonsRepository, PersonsRepository>();
+
+            services.AddDbContext<ContactsManagerDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection") ?? "");
+            });
+
+            return services;
+        }
+    }
+}
