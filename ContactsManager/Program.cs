e6e938fef3dfcdfd4cdde2e7b4b9c481bf7a7322
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-21 15:46:00 +0700

    Apply static function to add all services

--- ContactsManager/Program.cs
+++ ContactsManager/Program.cs
@@ -7,6 +7,7 @@ using ServicesContracts.Interfaces;
 using Serilog;
 using ContactsManager.Filters.ActionFilters;
 using ContactsManager.Filters.ResultFilters;
+using ContactsManager.StartupExtensions;
 
 namespace ContactsManager
 {
@@ -30,46 +31,48 @@ namespace ContactsManager
                     .ReadFrom.Services(services); //read out current app's services and make them available to serilog
             });
 
-            builder.Services.AddHttpLogging(logging =>
-            {
-                logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
-                logging.RequestHeaders.Add("sec-ch-ua");
-                logging.ResponseHeaders.Add("MyResponseHeader");
-                logging.MediaTypeOptions.AddText("application/javascript");
-                logging.RequestBodyLogLimit = 4096;
-                logging.ResponseBodyLogLimit = 4096;
-                logging.CombineLogs = true;
-            });
+            //builder.Services.AddHttpLogging(logging =>
+            //{
+            //    logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+            //    logging.RequestHeaders.Add("sec-ch-ua");
+            //    logging.ResponseHeaders.Add("MyResponseHeader");
+            //    logging.MediaTypeOptions.AddText("application/javascript");
+            //    logging.RequestBodyLogLimit = 4096;
+            //    logging.ResponseBodyLogLimit = 4096;
+            //    logging.CombineLogs = true;
+            //});
 
-            builder.Services.AddTransient<ResponseHeaderActionFilter>();
+            //builder.Services.AddTransient<ResponseHeaderActionFilter>();
 
-            builder.Services.AddControllersWithViews(options =>
-            {
-                //options.Filters.Add<ResponseHeaderActionFilter>(5); without any parameters
+            //builder.Services.AddControllersWithViews(options =>
+            //{
+            //    //options.Filters.Add<ResponseHeaderActionFilter>(5); without any parameters
 
-                var logger = builder.Services.BuildServiceProvider()
-                    .GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+            //    var logger = builder.Services.BuildServiceProvider()
+            //        .GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
 
-                options.Filters.Add(new ResponseHeaderActionFilter(logger)
-                {
-                    Key = "My-Key-From-Global",
-                    Value = "My-Key-From-Global",
-                    Order = 2
-                });
+            //    options.Filters.Add(new ResponseHeaderActionFilter(logger)
+            //    {
+            //        Key = "My-Key-From-Global",
+            //        Value = "My-Key-From-Global",
+            //        Order = 2
+            //    });
 
-                options.Filters.Add<PersonAlwaysRunResultFilter>();
-            });
+            //    options.Filters.Add<PersonAlwaysRunResultFilter>();
+            //});
 
-            builder.Services.AddScoped<ICountriesService, CountriesService>();
-            builder.Services.AddScoped<IPersonsService, PersonsService>();
+            //builder.Services.AddScoped<ICountriesService, CountriesService>();
+            //builder.Services.AddScoped<IPersonsService, PersonsService>();
 
-            builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
-            builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
+            //builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+            //builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
 
-            builder.Services.AddDbContext<ContactsManagerDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? "");
-            });
+            //builder.Services.AddDbContext<ContactsManagerDbContext>(options =>
+            //{
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? "");
+            //});
+
+            builder.Services.ConfigureServices(builder.Configuration);
 
             var app = builder.Build();
 
