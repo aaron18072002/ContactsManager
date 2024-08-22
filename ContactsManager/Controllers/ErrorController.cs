c030f0b3881d83fa598806e2651036214c547754
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-08-22 11:57:45 +0700

    Create view and endpoint for error handling

---
+++ ContactsManager/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature = this.HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            if(exceptionHandlerPathFeature is not null && exceptionHandlerPathFeature.Error is not null)
+            {
+                ViewBag.ErrorMessage = exceptionHandlerPathFeature.Error.Message;
+            }
+
+            return View();
+        }
+    }
+}
